d02cdd5010a00b08c0cba4c8bc9de106cbe30d56
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2021-06-18 11:04:46 +0200

    refined logging

--- ferrari-arg-auto/lib/src/main/java/sm/intersection/sim/Simulation.java
+++ ferrari-arg-auto/lib/src/main/java/sm/intersection/sim/Simulation.java
@@ -21,7 +21,7 @@ public final class Simulation {
 	private final Logger log = LoggerFactory.getLogger(Simulation.class);
 	private final SmartJunction junction;
 	private final List<CrossingCar> cars;
-	private long step;
+	private long step; // TODO make step configurable (X seconds/minutes/...)
 
 	public Simulation(final SmartJunction junction, List<CrossingCar> cars) {
 		this.junction = junction;
@@ -42,18 +42,22 @@ public final class Simulation {
 	}
 
 	public void logSituation() {
+		int nCars;
 		log.info("Logging simulation situation -----START----->");
 		System.out.printf("%s ways junction <%s> managed by policy <%s> \n", this.junction.nRoads(), this.junction.getName(),
 				this.junction.getPolicy().getName());
 		for (WAY way : this.junction.getRoads().keySet()) {
-			System.out.printf("\t %s: %d lanes \n", way, this.junction.getRoads().get(way).getRoad().nLanes(),
+			nCars = 0;
+			System.out.printf("\t %s: %d lane(s) \n", way, this.junction.getRoads().get(way).getRoad().nLanes(),
 					this.junction.getRoads().get(way).getRoad().getLanes());
 			for (CrossingCar car : this.cars) {
 				if (car.getWay().equals(way)) {
-					System.out.printf("\t\t Car <%s> %s in %f s (urgency: %f)\n", car.getName(), car.getState(),
-							car.getTimeToCross(), car.getCar().getUrgency());
+					nCars++;
+					System.out.printf("\t\t <%s> %s in %.2f s (urgency: %.2f) at %.2f km/h (distance: %.2f m) \n", car.getName(), car.getState(),
+							car.getTimeToCross(), car.getCar().getUrgency(), car.getCar().getCar().getSpeed(), car.getDistance());
 				}
 			}
+			System.out.printf("\t\t ----- %d car(s) \n", nCars);
 		}
 		log.info("<-----END----- Logging simulation situation");
 	}
