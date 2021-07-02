c153b1521239dac478d120d5155271aa838c5920
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2021-07-02 14:52:17 +0200

    added skeleton classes and comment

---
+++ ferrari-arg-auto/lib/src/main/java/sm/arg/general/ArgumentationGraph.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package sm.arg.general;
+
+import org.tweetyproject.arg.aspic.syntax.AspicArgumentationTheory;
+import org.tweetyproject.logics.pl.syntax.PlFormula;
+
+/**
+ * @author sm
+ *
+ */
+public final class ArgumentationGraph {
+    
+    private final AspicArgumentationTheory<PlFormula> argGraph;
+    
+    public ArgumentationGraph(AspicArgumentationTheory<PlFormula> argTheory) {
+        this.argGraph = argTheory;
+    }
+    
+    public void graph2text() {
+        // TODO 
+        return
+    }
+
+}
---
+++ ferrari-arg-auto/lib/src/main/java/sm/arg/intersection/ArgKeys.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package sm.arg.intersection;
+
+/**
+ * @author sm
+ *
+ */
+public enum ArgKeys {
+    
+    KEYWORD1, KEYWORD2, ...
+
+}
