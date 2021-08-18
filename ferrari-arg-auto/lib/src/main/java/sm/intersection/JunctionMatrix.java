26e97d850dffda84a428ea12beaf8e7f8ad2ec57
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2021-08-18 15:17:11 +0200

    tested next, works

--- ferrari-arg-auto/lib/src/main/java/sm/intersection/JunctionMatrix.java
+++ ferrari-arg-auto/lib/src/main/java/sm/intersection/JunctionMatrix.java
@@ -1,8 +1,13 @@
 package sm.intersection;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JunctionMatrix {
 
-    // private final Logger log = LoggerFactory.getLogger(JunctionMap.class);
+    private final Logger log = LoggerFactory.getLogger(JunctionMatrix.class);
     private final SmartJunction[][] junctions;
     private final int r;
     private final int c;
@@ -11,6 +16,14 @@ public class JunctionMatrix {
         this.r = junctions.length;
         this.c = junctions[0].length;
         this.junctions = junctions;
+    }
+
+    public int nRows() {
+        return this.r;
+    }
+
+    public int nCols() {
+        return this.c;
     }
 
     public SmartJunction getJunction(final int row, final int col) {
@@ -22,9 +35,71 @@ public class JunctionMatrix {
         return this;
     }
 
-    public SmartJunction next(final int[] coord, final WAY way, final DIRECTION dir) {
-        // TODO restituire nuovo incrocio sapendo strada di provenienza (way) e direzione (dir) dell'auto (direi che serva anche sapere in quale incrocio è l'auto nella mappa)
-        return null;
+    public Optional<SmartJunction> next(final int[] coord, final WAY way, final DIRECTION dir) {
+        Optional<SmartJunction> res;
+        int[] next = null;
+        switch (way) {
+            case WEST:
+                switch (dir) {
+                    case STRAIGHT:
+                        next = new int[] { coord[0], coord[1] + 1 };
+                        break;
+                    case RIGHT:
+                        next = new int[] { coord[0] + 1, coord[1] };
+                        break;
+                    case LEFT:
+                        next = new int[] { coord[0] - 1, coord[1] };
+                        break;
+                }
+                break;
+            case NORTH:
+                switch (dir) {
+                    case STRAIGHT:
+                        next = new int[] { coord[0] + 1, coord[1] };
+                        break;
+                    case RIGHT:
+                        next = new int[] { coord[0], coord[1] - 1 };
+                        break;
+                    case LEFT:
+                        next = new int[] { coord[0], coord[1] + 1 };
+                        break;
+                }
+                break;
+            case EAST:
+                switch (dir) {
+                    case STRAIGHT:
+                        next = new int[] { coord[0], coord[1] - 1 };
+                        break;
+                    case RIGHT:
+                        next = new int[] { coord[0] - 1, coord[1] };
+                        break;
+                    case LEFT:
+                        next = new int[] { coord[0] + 1, coord[1] };
+                        break;
+                }
+                break;
+            case SOUTH:
+                switch (dir) {
+                    case STRAIGHT:
+                        next = new int[] { coord[0] - 1, coord[1] };
+                        break;
+                    case RIGHT:
+                        next = new int[] { coord[0], coord[1] + 1 };
+                        break;
+                    case LEFT:
+                        next = new int[] { coord[0], coord[1] - 1 };
+                        break;
+                }
+                break;
+        }
+        try {
+            SmartJunction sm = this.getJunction(next[0], next[1]);
+            res = Optional.of(sm);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            this.log.warn("JUNCTION ({},{}) DOES NOT EXIST", next[0], next[1]);
+            res = Optional.empty();
+        }
+        return res;
     }
 
     @Override
--- ferrari-arg-auto/lib/src/test/java/sm/intersection/JunctionMatrixTest.java
+++ ferrari-arg-auto/lib/src/test/java/sm/intersection/JunctionMatrixTest.java
@@ -5,6 +5,8 @@ package sm.intersection;
 
 import static org.junit.Assert.*;
 
+import java.util.Optional;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,7 +29,7 @@ public class JunctionMatrixTest {
     @Before
     public void setUp() throws Exception {
         FourWaysJunctionConfig fourWays;
-        SmartJunction[][] junctions = new SmartJunction[4][4];
+        SmartJunction[][] junctions = new SmartJunction[2][2];
         for (int r = 0; r < junctions.length; r++) {
             for (int c = 0; c < junctions[r].length; c++) {
                 fourWays = new FourWaysJunctionConfig(
@@ -73,7 +75,52 @@ public class JunctionMatrixTest {
      */
     @Test
     public final void testNext() {
-        fail("Not yet implemented"); // TODO
+        for (int r = 0; r < this.jm.nRows(); r++) {
+            for (int c = 0; c < this.jm.nCols(); c++) {
+//                assertEquals(this.jm.getJunction(r, c+1), this.jm.next(new int[] {r, c}, WAY.WEST, DIRECTION.STRAIGHT));
+//                assertEquals(this.jm.getJunction(r+1, c), this.jm.next(new int[] {r, c}, WAY.WEST, DIRECTION.RIGHT));
+//                assertEquals(this.jm.getJunction(r-1, c), this.jm.next(new int[] {r, c}, WAY.WEST, DIRECTION.LEFT));
+//                assertEquals(this.jm.getJunction(r+1, c), this.jm.next(new int[] {r, c}, WAY.NORTH, DIRECTION.STRAIGHT));
+//                assertEquals(this.jm.getJunction(r, c-1), this.jm.next(new int[] {r, c}, WAY.NORTH, DIRECTION.RIGHT));
+//                assertEquals(this.jm.getJunction(r, c+1), this.jm.next(new int[] {r, c}, WAY.NORTH, DIRECTION.LEFT));
+//                assertEquals(this.jm.getJunction(r, c-1), this.jm.next(new int[] {r, c}, WAY.EAST, DIRECTION.STRAIGHT));
+//                assertEquals(this.jm.getJunction(r-1, c), this.jm.next(new int[] {r, c}, WAY.EAST, DIRECTION.RIGHT));
+//                assertEquals(this.jm.getJunction(r+1, c), this.jm.next(new int[] {r, c}, WAY.EAST, DIRECTION.LEFT));
+//                assertEquals(this.jm.getJunction(r-1, c), this.jm.next(new int[] {r, c}, WAY.SOUTH, DIRECTION.STRAIGHT));
+//                assertEquals(this.jm.getJunction(r, c+1), this.jm.next(new int[] {r, c}, WAY.SOUTH, DIRECTION.RIGHT));
+//                assertEquals(this.jm.getJunction(r, c-1), this.jm.next(new int[] {r, c}, WAY.SOUTH, DIRECTION.LEFT));
+                Optional<SmartJunction> opt = this.jm.next(new int[] {r, c}, WAY.WEST, DIRECTION.STRAIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.WEST, DIRECTION.RIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.WEST, DIRECTION.LEFT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.NORTH, DIRECTION.STRAIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.NORTH, DIRECTION.RIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.NORTH, DIRECTION.LEFT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.EAST, DIRECTION.STRAIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.EAST, DIRECTION.RIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.EAST, DIRECTION.LEFT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.SOUTH, DIRECTION.STRAIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.SOUTH, DIRECTION.RIGHT);
+                printNext(r, c, opt);
+                opt = this.jm.next(new int[] {r, c}, WAY.SOUTH, DIRECTION.LEFT);
+                printNext(r, c, opt);
+            }
+        }
+    }
+
+    private void printNext(int r, int c, Optional<SmartJunction> opt) {
+        if (!opt.isEmpty()) {
+            System.out.println(String.format("Current: (%d,%d) \t next: %s", r, c, opt.get().getName()));
+        }
     }
 
     /**
