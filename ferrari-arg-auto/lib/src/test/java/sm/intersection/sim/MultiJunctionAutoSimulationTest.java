0016c2452073d6eaed6c8a04ceeb45a68342037c
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2021-09-08 17:18:18 +0200

    random strat

--- ferrari-arg-auto/lib/src/test/java/sm/intersection/sim/MultiJunctionAutoSimulationTest.java
+++ ferrari-arg-auto/lib/src/test/java/sm/intersection/sim/MultiJunctionAutoSimulationTest.java
@@ -51,7 +51,7 @@ public class MultiJunctionAutoSimulationTest {
                  * ...and ONLY FOR (0,0) JUNCTION, for each WAY - DIRECTION combination...
                  */
                 //                if (r == 0 && c == 0) {
-                VehiclesGenStrategy strat = new DeepRouteMaxStrategy();
+                VehiclesGenStrategy strat = new DeepRouteRandomStrategy();
                 strat.configJunction(fourWays.getJunction());
                 strat.setSeed(1); // same seed = same random numbers
                 //                }
