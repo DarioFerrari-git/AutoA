7d2044321926f721b6b5565b3138ee6227e35546
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2021-10-22 12:08:18 +0200

    code works but plots should be different due to log scale

--- ferrari-arg-auto/dataAnalysis/code/plot_results.py
+++ ferrari-arg-auto/dataAnalysis/code/plot_results.py
@@ -7,12 +7,49 @@ import matplotlib.pyplot as plt
 plt.ioff()  # non interactive: plot to file, not screen
 matplotlib.use("PDF")
 
-for f in os.listdir("../results"):
-    if os.path.isdir(f):
+#print(os.getcwd())
 
+input_dir = "../results"
+data_filename = "performance.csv"
+props_filename = "sim_settings.properties"
+output_dir = "../plots"
 
-data = pd.read_csv('../results/1/performance.csv')
-#print(data.head())
+i = 0
+for f in os.listdir(input_dir):
+    data = pd.read_csv(f'{input_dir}/{f}/{data_filename}')
+    #print(data.head())
+    with open(f'{input_dir}/{f}/{props_filename}') as p:
+        grid = f'{p.readline().split("=")[1].strip()}x{p.readline().split("=")[1]}'
+        #print(grid)
+    plt.figure(i)
+    ax1 = plt.subplot(2, 1, 2)  # 2nd first to put xlabel
+    plt.plot(data["simulation_step"], data["vehicles"], marker="o", color="b")
+    plt.grid()
+    #plt.xticks(np.arange(0, 50, 5))
+    #plt.yticks(np.arange(0, 45, 5))
+    plt.ylabel("# vehicles")
+    plt.xlabel("simulation step")
+    ax2 = plt.subplot(2, 1, 1, sharex=ax1)  # 1st second to inherit xlabel
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.title(f"Performance per simulation step ({grid} network)", fontsize=10)  # title in 1st plot
+    plt.plot(data["simulation_step"], data["alternative_routes_used"], marker="^", color="g")
+    plt.grid()
+    #plt.yticks(np.arange(0, 40, 5))
+    plt.ylabel("# alt. routes")
+
+    # plt.show()
+    os.mkdir(f"{output_dir}/{grid}")
+    plt.savefig(f"{output_dir}/{grid}/performance_steps.pdf")
+
+    plt.figure(2)
+    plt.plot(data["vehicles"], data["alternative_routes_used"], marker="x", color="r")
+    plt.grid()
+    plt.title(f"Performance per number of vehicles ({grid} network)", fontsize=10)
+    plt.ylabel("# alt. routes")
+    plt.xlabel("# vehicles")
+
+    # plt.show()
+    plt.savefig(f"{output_dir}/{grid}/performance_vehicles.pdf")
 
 # OO interface (lower level, more flexibility)
 #fig, ax = plt.subplots()  # Create a figure containing a single axes.
@@ -20,31 +57,3 @@ data = pd.read_csv('../results/1/performance.csv')
 #ax.plot(data["simulation_step"], data["alternative_routes_used"], label="# alt. routes", marker="x")  # Plot some data on the axes.
 #ax.plot(data["simulation_step"], data["vehicles"], label="# vehicles", marker="o")
 # OR PLT interface
-plt.figure(1)
-ax1 = plt.subplot(2, 1, 2)  # 2nd first to put xlabel
-plt.plot(data["simulation_step"], data["vehicles"], marker="o", color="b")
-plt.grid()
-plt.xticks(np.arange(0, 50, 5))
-plt.yticks(np.arange(0, 45, 5))
-plt.ylabel("# vehicles")
-plt.xlabel("simulation step")
-ax2 = plt.subplot(2, 1, 1, sharex=ax1)  # 1st second to inherit xlabel
-plt.setp(ax2.get_xticklabels(), visible=False)
-plt.title("Performance per simulation step")  # title in 1st plot
-plt.plot(data["simulation_step"], data["alternative_routes_used"], marker="x", color="r")
-plt.grid()
-plt.yticks(np.arange(0, 40, 5))
-plt.ylabel("# alt. routes")
-
-#plt.show()
-plt.savefig("../plots/performance_steps.pdf")
-
-plt.figure(2)
-plt.plot(data["vehicles"], data["alternative_routes_used"], marker="^", color="g")
-plt.grid()
-plt.title("Performance per number of vehicles")
-plt.ylabel("# alt. routes")
-plt.xlabel("# vehicles")
-
-#plt.show()
-plt.savefig("../plots/performance_vehicles.pdf")
